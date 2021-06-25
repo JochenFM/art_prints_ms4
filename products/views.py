8579a2fb2503fe9aa2aae6093f0ca99dca765273
JochenFM <jochen_mayer@gmx.de>
2021-06-25 13:06:39 +0000

    first steps towards products view plus template products_onlineshop

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+
+def onlineshop(request):
+    """ a view to show all products in online shop  """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+    return render(request, 'products/products_onlineshop.html', context)
