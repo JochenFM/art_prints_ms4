a3d97992940109008661015fe86a3531a1ce40cf
JochenFM <jochen_mayer@gmx.de>
2021-07-01 10:05:00 +0000

    added cart context and delivery logic

--- art_prints/settings.py
+++ art_prints/settings.py
@@ -78,6 +78,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'cart.contexts.cart_contents',
             ],
         },
     },
@@ -163,6 +164,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MED_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
