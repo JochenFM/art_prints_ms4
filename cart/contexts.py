51abc969d915f48f07385196ede04c51c9d3ba42
JochenFM <jochen_mayer@gmx.de>
2021-07-06 17:54:48 +0000

    grand total and other price calculations now suddently work, but not the messages linked to it

--- cart/contexts.py
+++ cart/contexts.py
@@ -11,12 +11,10 @@ def cart_contents(request):
     product_count = 0
     cart = request.session.get('cart', {})
 
-
-
     for item_id, quantity in cart.items():
         product = get_object_or_404(Product, pk=item_id)
-        """total += quantity * product.price"""
-        """product_count += quantity"""
+        total += quantity * product.price
+        product_count += quantity
         cart_items.append({
             'item_id': item_id,
             'quantity': quantity,
@@ -32,7 +30,6 @@ def cart_contents(request):
 
     grand_total = delivery + total
 
-    
     context = {
         'cart_items': cart_items,
         'total': total,
