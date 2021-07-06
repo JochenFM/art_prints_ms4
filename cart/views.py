0940a455f952d17124d375418dc3492a86fd294c
JochenFM <jochen_mayer@gmx.de>
2021-07-06 15:58:49 +0000

    added toast and toast levels in base.html

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect, HttpResponse
+from django.contrib import messages
+
+from products.models import Product
 
 # Create your views here.
 
@@ -11,11 +14,13 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """add the specified product to the shopping cart"""
     
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))   
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     cart[item_id] = quantity
+    messages.success(request, f'Added {product.name} to your cart')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
