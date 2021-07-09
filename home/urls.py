eabd19309164310bef20f21c5dfefd6733b02398
JochenFM <jochen_mayer@gmx.de>
2021-07-09 11:32:00 +0000

    add class methods to webhook handler and webhook view from ado

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('', views.index, name='new_arrivals')
+    path('', views.new_arrivals, name='new_arrivals')
 ]
