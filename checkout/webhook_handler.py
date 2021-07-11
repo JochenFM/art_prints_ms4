92ca93932e10e0ace13c2014cc46f808237a4039
JochenFM <jochen_mayer@gmx.de>
2021-07-11 08:12:03 +0000

    added django country fields, bug fix webhook hanlder unsuccessful

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -18,7 +18,7 @@ class StripeWH_Handler:
         Handle a generic/unknown/unexpected webhook event
         """
         return HttpResponse(
-            content=f'Webhook received: {event["type"]}',
+            content=f'Unhandled webhook received: {event["type"]}',
             status=200)
 
     def handle_payment_intent_succeeded(self, event):
@@ -96,7 +96,8 @@ class StripeWH_Handler:
                             quantity=item_data,
                         )
                         order_line_item.save()
-    # if anything goes wrong, order is deleted if it was created by returning 500 to stripe                    
+    # if anything goes wrong, order is deleted if it was created by returning
+    # 500 to stripe         
             except Exception as e:
                 if order:
                     order.delete()
