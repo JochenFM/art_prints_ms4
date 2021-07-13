5b879a0d046c3cf42582262ec43d03019fbce69c
JochenFM <jochen_mayer@gmx.de>
2021-07-13 14:44:37 +0000

    bugfixing around the if/else of products with and without image - incomplete

--- products/views.py
+++ products/views.py
@@ -124,6 +124,7 @@ def edit_product(request, product_id):
 
     return render(request, template, context)
 
+
 @login_required
 def delete_product(request, product_id):
     """ Delete a product in the shop """
