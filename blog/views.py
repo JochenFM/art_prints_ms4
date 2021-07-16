bd9003e315de13182edf70ef9098fea7b470e7a2
JochenFM <jochen_mayer@gmx.de>
2021-07-16 08:29:50 +0000

    created blog app and first steps

---
+++ blog/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from 
+
+# Create your views here.
--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('', views.new_arrivals, name='new_arrivals')
+    path('home/', views.new_arrivals, name='new_arrivals')
 ]
