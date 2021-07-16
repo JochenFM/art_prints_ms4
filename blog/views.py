767a167540c51685c02280d87eaa4f12bb58cb84
JochenFM <jochen_mayer@gmx.de>
2021-07-16 10:51:28 +0000

    some dummy data for the blog.html

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.views.generic import ListView
 
 # Create your views here.
+posts = [
+     {
+         'author': 'JochenFM',
+         'title': 'Blog Post 1',
+         'content': 'First post content',
+         'date_posted': 'July 16, 2021',
+     }
+ ]
 
 
 def blog(request):
-    return render(request, 'blog/blog.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/blog.html', context)
