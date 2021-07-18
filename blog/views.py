4eb4486a22028cee49cec75d033cfd204208011e
JochenFM <jochen_mayer@gmx.de>
2021-07-18 16:32:18 +0000

    blog model added and basics for blog. html

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from .models import Post
 
 # Create your views here.
-posts = [
-     {
-         'author': 'JochenFM',
-         'title': 'Blog Post 1',
-         'content': 'First post content',
-         'date_posted': 'July 16, 2021',
-     }
- ]
 
 
 def blog(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/blog.html', context)
