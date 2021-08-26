e76ece5a117210e7a5246ee4cfda4fa359238404
JochenFM <jochen_mayer@gmx.de>
2021-08-26 13:53:44 +0000

    small stylistic improvements and created 404 and 500 error  html templates

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.views.generic import (
     ListView, 
     DetailView, 
@@ -26,13 +27,19 @@ class PostListView(ListView):
     ordering = ['-date_posted']
     paginate_by = 4
 
+# to filter and display posts created by a certain user
+
 
 class UserPostListView(ListView):
     model = Post
-    template_name = 'blog/blog.html'  # name convention is <app>/<model>_<viewtype>.html
+    template_name = 'blog/user_posts.html'
     context_object_name = 'posts'
-    ordering = ['-date_posted']
-    paginate_by = 4
+    paginate_by = 2
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
 
 class PostDetailView(DetailView):
     model = Post
