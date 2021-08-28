4a89734a03f4ebef78a97cc194830464d7bb7c96
JochenFM <jochen_mayer@gmx.de>
2021-08-28 11:47:19 +0000

    debug back to 'DEVELOPMENT' and some tidying up inblog  models.py

--- blog/models.py
+++ blog/models.py
@@ -3,22 +3,17 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    """category = models.CharField(max_length=254)
-    updated_on = models.DateTimeField(auto_now=True)"""
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
-    
-    """def __str__(self):
-        return '{}, {}'.format(self.title,
-                               self.category)"""
 
 
