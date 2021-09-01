6f38e0c171bdfd6bb352bcd3fb3dace3e7d59a0e
JochenFM <jochen_mayer@gmx.de>
2021-09-01 07:45:52 +0000

    committing to see whether blog now works on heroku app

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=199)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
--- home/views.py
+++ home/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
-from .models import Product, Category
+from products.models import Product, Category
 
 
 def index(request):
     """ a view to return the index page and display products of
     new_arrivals category """
     
-    
-    products = Product.objects.all().filter(category__name__in=new_arrivals)
-    print("filter products", products)
-    
+    products = Product.objects.all().filter(category__name="new_arrivals")
+    print("products", products)
+
+    context = {
+        'products': products,
+    }
     return render(request, 'home/index.html', context)
 
